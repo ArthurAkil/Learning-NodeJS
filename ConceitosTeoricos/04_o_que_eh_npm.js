e544bc2a9caf280817bf66920a993c1e80627666
Arthur A. <arthurakilalves@gmail.com>
2025-10-14 15:44:07 -0300

    O que é NPM

---
+++ ConceitosTeoricos/04_o_que_eh_npm.js
@@ -0,0 +1,11 @@
+// O npm
+
+// Ferramenta utilizada para instalar módulos de JavaScript
+
+// Que podem fazer desde operações matemáticas até comunicação HTTP
+
+// Instalamos um pacote pelo comando: npm install pacote
+
+// podemos instalar o npm install express uma ferramenta que ajuda no desenvolvimento de um api
+
+// contudo, se instalarmos ela só, ela apontará erros pois necessita de outras coisas para funcionar corretamente
