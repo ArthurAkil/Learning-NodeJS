e8dcbf106ded35c6ef665c85d6a0833f5b7ac961
Arthur A. <arthurakilalves@gmail.com>
2025-10-14 16:12:17 -0300

    O que é uma api

---
+++ ConceitosTeoricos/08_o_que_eh_api.js
@@ -0,0 +1,15 @@
+// O que é uma API?
+
+// Application Programming interface
+
+// Uma maneira de se comunicar via HTTP a um servidor e fazer operações como: visualizar, deletar, criar e atualizar dados
+
+// CRUD (create, read, update, delete)
+
+// Grandes aplicações como facebook e instagram utilizam APIs
+
+// Grande parte dos softwares desenvolvidos em node.js tem uma API
+
+// E utilizam um framwork chamado Express para isso
+
+// Uma forma de se comunicar http com o servidor de uma forma que o codigo do frontend fique separado do backend, separando a lógica
