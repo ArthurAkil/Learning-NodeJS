78485139f971ab8283660c1804aeddc53aa239a2
Arthur A. <arthurakilalves@gmail.com>
2025-10-15 11:39:20 -0300

    framework express

---
+++ ConceitosTeoricos/11_express.js
@@ -0,0 +1,9 @@
+// Express
+
+// Um framework web, muito utilizado para criar aplicações em Node.js
+
+// Utiliza em seu core o módulo HTTP para criar as comunicações
+
+// Além de outros módulos
+
+// Podemos criar uma API com o Express facilmente
