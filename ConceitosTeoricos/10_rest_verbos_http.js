cf56c3820327921ba1baa3f88331a0c90efb8317
Arthur A. <arthurakilalves@gmail.com>
2025-10-15 11:39:08 -0300

    verbos http

---
+++ ConceitosTeoricos/10_rest_verbos_http.js
@@ -0,0 +1,11 @@
+// REST e os verbos HTTP
+
+// Sempre que criamos uma API devemos utilizar os verbos HTTP corretos para cada url da nossa aplicação (veremos isso na prática)
+
+// Verboo GET: para solicitar dados
+
+// Verbo POST: para inserir dados
+
+// Verbo DELETE: para deletar dados
+
+// Verbo PUT: para atualizar dados
