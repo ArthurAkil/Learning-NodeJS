f064dc0f0c0b67ec42fa892c229d9b1536df3283
Arthur A. <arthurakilalves@gmail.com>
2025-10-16 01:36:20 -0300

    feat: adificionando rotas no arquivo principal

--- Projetos/JobFinder/app.js
+++ Projetos/JobFinder/app.js
@@ -4,11 +4,16 @@ const app = express();
 
 const db = require("./db/connection");
 
+const bodyParser = require("body-parser");
+
 const PORT = 3000;
 
 app.listen(PORT, () => {
   console.log(`O servidor está rodando na porta: ${PORT}`);
 });
+
+// body_parser
+app.use(bodyParser.urlencoded({ extended: false })); // agora estamos utilizando o bodyparser, então a parte de vir algo do body nas rotas 'request.body' (routes/jobs.js) quando envio informação já está funcionando agr
 
 // db connection
 // O authenticate vai fazer o teste para a conexão com o banco, ou quando tiver um crud dentro desse banco
@@ -24,6 +29,12 @@ db.authenticate()
 app.get("/", (request, response) => {
   response.send("Está funcionando 2");
 });
+
+// jobs routes
+
+app.use("/jobs", require("./routes/jobs"));
+// (criamos as rotas em arquivos separados e precisamos puxar ele para o app.js arquivo principal)
+// basicamente deixando assim fica /jobs/add/
 
 // antes iniciariamos com node app.js
 // mas ai para ver qualquer atualização precisariamos parar o servidor e rodar dnv, com o nodemon não precisamos disso
