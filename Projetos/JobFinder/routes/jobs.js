45906bbbbb857ef44e61c5b7cf2740280e1573f2
Arthur A. <arthurakilalves@gmail.com>
2025-10-16 01:36:01 -0300

    feat: adicionando rotas especificas de jobs

---
+++ Projetos/JobFinder/routes/jobs.js
@@ -0,0 +1,31 @@
+const express = require("express"); // precisamos do express pois ele quem cuide de rotas
+
+const router = express.Router(); // precisamos do objeto de rotas do express
+
+const Job = require("../models/Job"); // e precisamos do model que criamos
+
+router.get("/test", (req, res) => {
+  res.send("Deu certo");
+});
+
+// adicionar job POST
+router.post("/add", (request, response) => {
+  // para adicionar um job precisaremos de um corpo com todos os dados do Job
+
+  let { title, salary, company, description, email, new_job } = request.body;
+
+  // insert objetos
+  Job.create({
+    //isso é uma promesa, podemos usar o then
+    title,
+    description,
+    salary,
+    company,
+    email,
+    new_job,
+  })
+    .then(() => response.redirect("/"))
+    .catch((error) => console.log(error));
+});
+
+module.exports = router;
