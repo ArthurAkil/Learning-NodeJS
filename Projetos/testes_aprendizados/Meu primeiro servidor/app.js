2048df5e2033ebaa003f2c5fd361c472e4826f1a
Arthur A. <arthurakilalves@gmail.com>
2025-10-17 18:47:31 -0300

    feat: adicionando crud de rotas

--- Projetos/testes_aprendizados/Meu primeiro servidor/app.js
+++ Projetos/testes_aprendizados/Meu primeiro servidor/app.js
@@ -17,6 +17,98 @@ const app = express();
 // 2 passando o express pra dentro de outra constante, é com ela que vamos construir nosso servidor
 //--------------------------------------------------------------
 
+// 13. importando Produto.js
+const Produtos = require("./models/Produtos");
+
+// 15. configurando body-parser
+const bodyParser = require("body-parser");
+// 15.1 configurando o bodyParser:
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// 14. criando rota post que cria um produto
+
+app.post("/cadastro", (request, response) => {
+  Produtos.create({
+    // nome: request.body.nome -> não ira reconhecer pois não foi instalado ainda a biblioteca body parser
+    // (npm instal --save body-parser) (rebece as informações dos formularios e faz a conversão em json)
+    nome: request.body.nome,
+    preco: request.body.preco,
+    descricao: request.body.descricao,
+  })
+    .then(() => {
+      console.log("Produto criado com sucesso"); //em caso de erro, log(sucesso), e mande de volta uma resposta informando que foi criado
+      response.send("Produto criado com sucesso");
+    })
+    .catch((error) => {
+      console.log("Error" + error);
+      response.send("Error" + error);
+    }); //se deu erro, log(error)
+});
+
+// 14.2 criando rota get
+app.get("/", (req, res) => {
+  Produtos.findAll() // metodo do sequelize para pegar todos os Produtos cadastrados no banco de dados
+    .then((produtos) => {
+      //como é uma promise nos temos o then e catch e caso dê certo, ele vai receber como parâmetros os produtos e nós mandamos como resposta os produtos(valor) numa chave produtos (tudo isso por causa do body-parser que transforma o resultado retornando/enviando em json)
+      res.send({ protudos: produtos }); //ou posso mandar direto os produtos res.send(produtos)
+    })
+    .catch((error) => {
+      console.log("Deu erro" + error);
+      res.send("Erro ao buscar os dados" + error);
+    });
+});
+
+// 15 criando rota de pesquisa por nome de produto GET - read
+app.get("/:nome", (req, res) => {
+  Produtos.findAll({ where: { nome: req.params.nome } })
+    .then((produto) => {
+      res.send(produto);
+      console.log("Consulta realizada com sucesso");
+    })
+    .catch((error) => {
+      res.send("Produto não existe na base de dados" + error);
+      console.log("Produto não existe na base de dados" + error);
+    });
+});
+
+// 14.3 atualizando um produto PUT(atualizar tudo) OU PATCH(geralmente atualizar poucas informações ou todas se forem passadas)
+app.patch("/atualizar/:id", (req, res) => {
+  // passamos como parametro o id que queremos que seja atualizado
+  Produtos.update(
+    {
+      // como colocamos nome e preco como allowNull: false, eles obrigatoriamente devem ser colocados
+      nome: req.body.nome,
+      preco: req.body.preco,
+      descricao: req.body.descricao,
+    },
+    // o segundo parâmetro do update, é um where: onde dizemos que o Produto que vai ser atualizado é aquele que possui o id no parâmetro
+    { where: { id: req.params.id } }
+  )
+    .then(() => {
+      res.send("Sucesso ao atualizar o produto.");
+      console.log("Foi atualizado com sucesso");
+    })
+    .catch((error) => {
+      res.send("Error ao atualizar o produto" + error);
+    });
+});
+
+// 14.4 delete - metodo Http da rota
+app.delete("/deletar/:id", (req, res) => {
+  Produtos.destroy({ where: { id: req.params.id } }) // metodo do banco de dados de deletar algo do banco de dados, mesma lógica do patch, colocamos um where para identificar qual id do produto que queremos deletar (o passado no parâmetro)
+    .then(() => {
+      res.send("Produto deletado com sucesso");
+      console.log("Produto deletado");
+    })
+    .catch((error) => {
+      res.send("Error ao deletar o produto" + error);
+      console.log("Erro ao deletar o produto" + error);
+    });
+});
+
+//--------------------------------------------------------------
+
 // 3 coloquei a porta numa variavel para melhor acesso
 const PORT = 8081;
 // 3.1 agora colocamos para nosso aplicativo escutar uma porta com o listen() que recebe como parâmetros a porta para acessar, e uma função
@@ -34,25 +126,21 @@ app.listen(PORT, () => {
 // 4.1 utilizamos o get do express e nele colocamos dois parâmetros, o primeiro parâmetro é em string "/" informando a rota, no caso da / isolada é considerada a home
 // 4.2 no segundo parâmetro é colocado uma função que recebe dois parâmetros, request -> feita pelo usario que ta usando o sistema, response -> resposta do servidor para o usuario, e o escopo da função é a logica aplicada ou o que vai aparecer na tela
 // 4.3 o response.send("texto") envia uma mensagem direta no seco, não é um html bonitinho nem nada, esse bloco de código é usado para mandar objetos guardados no banco de dados para o frontend
-app.get("/", (request, response) => {
-  response.send("Você está na home");
-});
+// app.get("/", (request, response) => {
+//   response.send("Você está na home");
+// });
 
-app.get("/artigos", (request, response) => {
-  response.send("Todos os artigos, rota artigos");
-});
-
-app.get("/contato", (req, res) => {
-  res.send("Você está na rota contato");
-});
+// app.get("/artigos", (request, response) => {
+//   response.send("Todos os artigos, rota artigos");
+// });
 
 // 4.4 parametros na rota
 // 4.4.1 estamos passando como parâmetro um id em /artigos/:id -> significa que queremos um artigo específico daquele grupo de artigos
 // 4.4.2 o parâmetro da requisição feita pode ser acessada por meio do request, buscando os parâmetros nele (params) -> request.params.id
-app.get("/artigos/:id", (request, response) => {
-  if (request.params.id == "1") {
-    response.send("1 - Artigo como criar sistemas");
-  } else {
-    response.send("Não temos esse artigo com essa id");
-  }
-});
+// app.get("/artigos/:id", (request, response) => {
+//   if (request.params.id == "1") {
+//     response.send("1 - Artigo como criar sistemas");
+//   } else {
+//     response.send("Não temos esse artigo com essa id");
+//   }
+// });
