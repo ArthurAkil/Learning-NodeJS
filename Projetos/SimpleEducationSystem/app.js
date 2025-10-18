7a91b63991066584e1fc593fc0b8099e35c15856
Arthur A. <arthurakilalves@gmail.com>
2025-10-18 00:37:19 -0300

    feat: atualizando app.js para sincronizar os dados do db ler json

--- Projetos/SimpleEducationSystem/app.js
+++ Projetos/SimpleEducationSystem/app.js
@@ -2,6 +2,26 @@ const express = require("express");
 const app = express();
 const PORT = 8081;
 
+// Importando a conexão do banco -> garantie que os models sejam carregados
+const { sequelize } = require("./db/connection");
+
+// Sincronizando os modelos para criar a tabela
+require("./models/Aluno");
+require("./models/Professor");
+require("./models/Coordenador");
+
+sequelize
+  .sync({ force: false }) // false? se for true ele força a criação na tabela sempre
+  .then(() => {
+    console.log("Modelos sincronizados com o banco.");
+  })
+  .catch((error) => {
+    console.log("Erro ao sincronizar" + error);
+  });
+
+app.use(express.json()); // não preciso utilizar mais o body-parser pois já foi integrado ao express
+app.use(express.urlencoded({ extended: true }));
+
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta: ${PORT}`);
   console.log(`Acesse: http://localhost:${PORT}/`);
