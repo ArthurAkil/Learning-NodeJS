61328d2985e9475f874db81df7482216205c628c
Arthur A. <arthurakilalves@gmail.com>
2025-10-27 12:31:08 -0300

    feat: criando class app

---
+++ Projetos/testes_aprendizados/Segundo servidor/src/app.js
@@ -0,0 +1,25 @@
+// Parte de importação
+const express = require("express");
+const routes = require("./routes");
+
+// Classe onde vai conter a lógica
+class App {
+  constructor() {
+    this.server = express();
+    this.middlewares();
+    this.routes();
+  }
+
+  //camada que adicionamos entre o express e entre a aplicação, camada de interceptação, podendo aprovar a requisição ou não
+  middlewares() {
+    this.server.use(express.json());
+  }
+
+  routes() {
+    this.server.use(routes);
+  }
+}
+
+// Tudo que vamos exportar que outros arquivos vão usar, só preciso do server do app
+const app = new App();
+module.exports = app.server;
